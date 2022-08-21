b61557b24ed4710bf6016062167857ebdd80866a
kayaba <kayaba.fluctlight@gmail.com>
2022-08-21 17:13:29 +0700

    embed Poppin font, hide default Navigation Header

--- src/stack/index.tsx
+++ src/stack/index.tsx
@@ -3,11 +3,13 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import DashboardScreen from '../screens/Dashboard';
 
+import { screenOptions } from './shared';
+
 const Stack = createStackNavigator();
 
 export const BuildStack: FC = () => {
 	return (
-		<Stack.Navigator>
+		<Stack.Navigator screenOptions={screenOptions}>
 			<Stack.Screen name="Dashboard" component={DashboardScreen} />
 		</Stack.Navigator>
 	);
--- src/stack/shared.ts
+++ src/stack/shared.ts
@@ -1,3 +1,10 @@
+import { StackNavigationOptions } from '@react-navigation/stack';
+
+export const screenOptions: StackNavigationOptions = {
+	headerShown: false,
+	animationEnabled: false,
+};
+
 export interface BuildParamList {
 	Dashboard: undefined;
 }
