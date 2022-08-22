2d47326d8766fe51d0a9e845f32f72e38342355d
kayaba <kayaba.fluctlight@gmail.com>
2022-08-23 01:33:35 +0700

    sample SVG icons

---
+++ src/components/icons/feather/Info.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import { ViewStyle } from 'react-native';
+import { Circle, Line, Svg } from 'react-native-svg';
+
+interface Props {
+	style?: ViewStyle;
+	size?: number;
+	color?: string;
+}
+
+export const InfoIcon: FC<Props> = ({
+	style,
+	size = 16,
+	color = '#666666',
+}) => {
+	return (
+		<Svg
+			style={style}
+			width={size}
+			height={size}
+			fill="none"
+			stroke={color}
+			strokeWidth={2}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			viewBox="0 0 24 24"
+		>
+			<Circle cx={12} cy={12} r={10} />
+			<Line x1={12} y1={16} x2={12} y2={12} />
+			<Line x1={12} y1={8} x2={12.01} y2={8} />
+		</Svg>
+	);
+};
+
+export default InfoIcon;
