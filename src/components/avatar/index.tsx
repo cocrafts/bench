96390d8331f240de6077f261589d7edd577e98ab
Tinh Nguyen <nguythaitinh@gmail.com>
2022-08-28 02:10:00 +0700

    Add avatar component

---
+++ src/components/avatar/index.tsx
@@ -0,0 +1,64 @@
+import React, { FC } from 'react';
+import {
+	Image,
+	ImageStyle,
+	StyleSheet,
+	Text,
+	TextStyle,
+	View,
+	ViewStyle,
+} from 'react-native';
+
+interface Props {
+	uri?: string;
+	userName?: string;
+	size?: number;
+}
+
+export const Avatar: FC<Props> = ({ uri, userName, size = 22 }) => {
+	const imageStyle: ImageStyle = {
+		width: size,
+		height: size,
+		borderRadius: size / 2,
+	};
+
+	const containerStyle: ViewStyle = {
+		width: size,
+		height: size,
+		borderRadius: size / 2,
+	};
+
+	const textStyle: TextStyle = {
+		fontSize: (size * 12) / 22,
+		fontWeight: '400',
+	};
+
+	if (uri) {
+		return (
+			<Image
+				style={imageStyle}
+				source={{
+					uri: uri,
+				}}
+			/>
+		);
+	} else if (userName) {
+		return (
+			<View style={[containerStyle, styles.container]}>
+				<Text style={textStyle}>{userName.charAt(0)}</Text>
+			</View>
+		);
+	} else {
+		return null;
+	}
+};
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#7FC4BC',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
+
+export default Avatar;
