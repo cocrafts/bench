3fdd43d07942a5cb706032ffc5b670aba0fa179c
Tinh Nguyen <nguythaitinh@gmail.com>
2022-09-04 15:07:33 +0700

    Navigate to Sign in Screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const SignInScreen = () => {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<Text>Sign In</Text>
+		</View>
+	);
+};
+export default SignInScreen;
