b404ad312feb71c32cfab489de2183c676caeb27
Tinh Nguyen <nguythaitinh@gmail.com>
2022-09-08 23:53:11 +0700

    Add mockup data for reply field

--- src/utils/mockupData.ts
+++ src/utils/mockupData.ts
@@ -25,6 +25,17 @@ export const threads: Array<Thread> = [
 		isPinned: true,
 		isFollowed: true,
 		isLiked: true,
+		replies: [
+			{
+				name: 'Elon Musk',
+				content:
+					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+				avatarUrl:
+					'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+				postedTime: '2022-09-01T10:17:28.593Z',
+				nbLikes: 12,
+			},
+		],
 	},
 	{
 		name: 'Tinh Nguyen',
@@ -39,5 +50,212 @@ export const threads: Array<Thread> = [
 		isPinned: false,
 		isFollowed: false,
 		isLiked: false,
+		replies: [
+			{
+				name: 'meo',
+				content:
+					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+				avatarUrl: 'https://avatars.githubusercontent.com/u/11600914?v=4',
+				postedTime: '2022-09-01T10:17:28.593Z',
+				nbLikes: 12,
+				originReply: {
+					name: 'Elon Musk',
+					content:
+						'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+					avatarUrl:
+						'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+					postedTime: '2022-09-01T10:17:28.593Z',
+				},
+			},
+		],
+	},
+	{
+		name: 'Goku',
+		avatarUrl:
+			'https://static.game24h.vn/upload/2016/2016-4/2016-11-03/songoku.png',
+		thread:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
+		nbLikes: 1200,
+		nbComments: 163,
+		postedTime: '2022-09-01T10:17:28.593Z',
+		id: '3',
+		isPinned: false,
+		isFollowed: true,
+		isLiked: true,
+		replies: [
+			{
+				name: 'meo',
+				content:
+					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+				avatarUrl: 'https://avatars.githubusercontent.com/u/11600914?v=4',
+				postedTime: '2022-09-01T10:17:28.593Z',
+				nbLikes: 12,
+				originReply: {
+					name: 'Elon Musk',
+					content:
+						'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+					avatarUrl:
+						'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+					postedTime: '2022-09-01T10:17:28.593Z',
+				},
+			},
+		],
+	},
+	{
+		name: 'Shinichi',
+		avatarUrl: 'https://game8.vn/media/202101/images/4(8).jpg',
+		thread:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
+		nbLikes: 1200,
+		nbComments: 163,
+		postedTime: '2022-09-01T10:17:28.593Z',
+		id: '4',
+		isPinned: false,
+		isFollowed: true,
+		isLiked: true,
+		replies: [
+			{
+				name: 'meo',
+				content:
+					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+				avatarUrl: 'https://avatars.githubusercontent.com/u/11600914?v=4',
+				postedTime: '2022-09-01T10:17:28.593Z',
+				nbLikes: 12,
+				originReply: {
+					name: 'Elon Musk',
+					content:
+						'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+					avatarUrl:
+						'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+					postedTime: '2022-09-01T10:17:28.593Z',
+				},
+			},
+		],
+	},
+	{
+		name: 'Son Le',
+		avatarUrl: 'https://avatars.githubusercontent.com/u/11600914?v=4',
+		thread:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
+		nbLikes: 1200,
+		nbComments: 163,
+		postedTime: '2022-09-01T10:17:28.593Z',
+		id: '5',
+		isPinned: true,
+		isFollowed: true,
+		isLiked: true,
+		replies: [
+			{
+				name: 'meo',
+				content:
+					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+				avatarUrl: 'https://avatars.githubusercontent.com/u/11600914?v=4',
+				postedTime: '2022-09-01T10:17:28.593Z',
+				nbLikes: 12,
+				originReply: {
+					name: 'Elon Musk',
+					content:
+						'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+					avatarUrl:
+						'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+					postedTime: '2022-09-01T10:17:28.593Z',
+				},
+			},
+		],
+	},
+	{
+		name: 'Tinh Nguyen',
+		avatarUrl:
+			'https://ca.slack-edge.com/T16AT245P-U027NNBM1A7-8cfc5e4a99a4-512',
+		thread:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
+		nbLikes: 13,
+		nbComments: 12,
+		postedTime: '2022-08-28T10:17:28.593Z',
+		id: '6',
+		isPinned: false,
+		isFollowed: false,
+		isLiked: false,
+		replies: [
+			{
+				name: 'meo',
+				content:
+					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+				avatarUrl: 'https://avatars.githubusercontent.com/u/11600914?v=4',
+				postedTime: '2022-09-01T10:17:28.593Z',
+				nbLikes: 12,
+				originReply: {
+					name: 'Elon Musk',
+					content:
+						'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+					avatarUrl:
+						'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+					postedTime: '2022-09-01T10:17:28.593Z',
+				},
+			},
+		],
+	},
+	{
+		name: 'Goku',
+		avatarUrl:
+			'https://static.game24h.vn/upload/2016/2016-4/2016-11-03/songoku.png',
+		thread:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
+		nbLikes: 1200,
+		nbComments: 163,
+		postedTime: '2022-09-01T10:17:28.593Z',
+		id: '7',
+		isPinned: false,
+		isFollowed: true,
+		isLiked: true,
+		replies: [
+			{
+				name: 'meo',
+				content:
+					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+				avatarUrl: 'https://avatars.githubusercontent.com/u/11600914?v=4',
+				postedTime: '2022-09-01T10:17:28.593Z',
+				nbLikes: 12,
+				originReply: {
+					name: 'Elon Musk',
+					content:
+						'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+					avatarUrl:
+						'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+					postedTime: '2022-09-01T10:17:28.593Z',
+				},
+			},
+		],
+	},
+	{
+		name: 'Shinichi',
+		avatarUrl: 'https://game8.vn/media/202101/images/4(8).jpg',
+		thread:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
+		nbLikes: 1200,
+		nbComments: 163,
+		postedTime: '2022-09-01T10:17:28.593Z',
+		id: '8',
+		isPinned: false,
+		isFollowed: true,
+		isLiked: true,
+		replies: [
+			{
+				name: 'meo',
+				content:
+					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+				avatarUrl: 'https://avatars.githubusercontent.com/u/11600914?v=4',
+				postedTime: '2022-09-01T10:17:28.593Z',
+				nbLikes: 12,
+				originReply: {
+					name: 'Elon Musk',
+					content:
+						'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+					avatarUrl:
+						'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+					postedTime: '2022-09-01T10:17:28.593Z',
+				},
+			},
+		],
 	},
 ];
