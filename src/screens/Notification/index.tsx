df2e64eab9a6b87742d10db6babf02fac7ea161d
Tinh Nguyen <nguythaitinh@gmail.com>
2022-09-19 02:25:51 +0700

    Add search modal to notification screens

--- src/screens/Notification/index.tsx
+++ src/screens/Notification/index.tsx
@@ -1,24 +1,50 @@
-import React, { FC } from 'react';
-import { StyleSheet, View } from 'react-native';
-import ControllerRow from 'components/ControllerRow';
+import React, { FC, useEffect, useState } from 'react';
+import { ActivityIndicator, Modal, StyleSheet, View } from 'react-native';
 
+import ControllerRow from '../../components/ControllerRow';
+import SearchModal from '../../components/SearchModal';
 import { blackPearl, blue } from '../../utils/colors';
+import { mockupNotifications } from '../../utils/mockupData';
+import { Notification } from '../../utils/types';
 
 interface Props {}
 
 const NotificationScreen: FC<Props> = ({}: Props) => {
+	const [notifications, setNotifications] = useState<Array<Notification>>([]);
+	const [isSearchModalVisible, setIsSearchModalVisible] = useState(false);
+	const onCloseSearchModal = () => setIsSearchModalVisible(false);
+	const onSearchPress = () => setIsSearchModalVisible(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			return setNotifications(mockupNotifications);
+		}, 1000);
+	}, []);
+
 	return (
 		<View style={styles.container}>
+			<Modal visible={isSearchModalVisible} animationType={'slide'}>
+				<SearchModal onCancelSearchModal={onCloseSearchModal} />
+			</Modal>
 			<ControllerRow
 				bellIconColor={blue}
 				canGoBack={true}
 				onAvatarPress={function (): void {
 					throw new Error('Function not implemented.');
 				}}
-				onSearchPress={function (): void {
-					throw new Error('Function not implemented.');
-				}}
+				onSearchPress={onSearchPress}
 			/>
+			{notifications.length === 0 && (
+				<View style={styles.activityIndicatorContainer}>
+					<ActivityIndicator />
+				</View>
+			)}
+			{/* <FlatList
+				data={notifications}
+				renderItem={renderItem}
+				keyExtractor={(item) => item.id}
+				extraData={selectedId}
+			/> */}
 		</View>
 	);
 };
@@ -29,6 +55,9 @@ const styles = StyleSheet.create({
 		backgroundColor: blackPearl,
 		paddingTop: 32,
 		paddingHorizontal: 16,
+	},
+	activityIndicatorContainer: {
+		marginTop: 24,
 	},
 });
 
