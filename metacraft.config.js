5041c20c979db3d87a989e8825a745b8e228fd5c
kayaba <kayaba.fluctlight@gmail.com>
2022-11-07 16:45:38 +0700

    use local assets instead of cdn

--- metacraft.config.js
+++ metacraft.config.js
@@ -39,27 +39,12 @@ const copyAssets = (configs) => {
 	return configs;
 };
 
-const externals = (configs) => {
-	configs.externals = {
-		rxjs: 'rxjs',
-		react: 'React',
-		lodash: '_',
-		'amazon-cognito-identity-js': 'AmazonCognitoIdentity',
-		'react-dom': 'ReactDOM',
-		'react-art': 'ReactART',
-		'@blocto/sdk': 'BloctoSDK',
-		'@solana/web3.js': 'solanaWeb3',
-	};
-
-	return configs;
-};
-
 module.exports = {
 	useBabel: true,
 	publicPath: () => process.env.PUBLIC_URL || '/',
 	keepPreviousBuild: () => true,
 	buildId: () => 'app',
-	webpackMiddlewares: [setEnvironments, copyAssets, externals],
+	webpackMiddlewares: [setEnvironments, copyAssets],
 	moduleAlias: {
 		global: {
 			'react-native': 'react-native-web',
