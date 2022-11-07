2fc36f73bc8820749ec6946c61e71c13d1edcf19
kayaba <kayaba.fluctlight@gmail.com>
2022-11-07 20:12:54 +0700

    refactor app valtio state, eleminate user props (stored on account state)

--- src/utils/state/app/instance.ts
+++ src/utils/state/app/instance.ts
@@ -1,16 +1,15 @@
 import { WalletAdapterNetwork as Network } from '@solana/wallet-adapter-base';
 import config from 'utils/config';
-import { Profile } from 'utils/types/graphql';
 import { proxy } from 'valtio';
 
 export interface AppState {
 	counter: number;
-	user: Profile | null;
+	privacy: boolean;
 	network: Network;
 }
 
 export const appState = proxy<AppState>({
 	counter: 0,
-	user: null,
+	privacy: false,
 	network: config.defaultNetwork,
 });
