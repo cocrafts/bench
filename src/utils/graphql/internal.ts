df72a95ec1787104814a474aecab778b22b0ce3c
kayaba <fluctlight-kayaba@gmail.com>
2022-11-19 14:08:11 +0700

    cache manipulation boilerplate

---
+++ src/utils/graphql/internal.ts
@@ -0,0 +1,33 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import configs from 'utils/config';
+import { extractJwt } from 'utils/lib';
+
+const uri = !__DEV__
+	? 'http://localhost:3005/graphql'
+	: 'https://api.stormgate.io/graphql';
+
+const basicLink = new HttpLink({ uri, fetch });
+const authLink = setContext(async (_, { headers: originalHeaders }) => {
+	const token = await extractJwt();
+	const headers = {
+		'client-key': configs.clientKey,
+		...originalHeaders,
+	};
+
+	if (token) {
+		headers['Authorization'] = `Bearer ${token}`;
+	}
+
+	return { headers };
+});
+
+const httpLink = authLink.concat(basicLink);
+
+export const memoryCache = new InMemoryCache();
+
+export const graphQlClient = new ApolloClient({
+	link: httpLink,
+	cache: memoryCache,
+	defaultOptions: {},
+});
