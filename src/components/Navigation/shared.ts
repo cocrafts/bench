ea433cb3fdf6288168ab6a97775913dd0d0f6f87
FME849 <1414439@gmail.com>
2022-12-08 17:25:03 +0700

    Update link in navigation

--- src/components/Navigation/shared.ts
+++ src/components/Navigation/shared.ts
@@ -14,8 +14,8 @@ export const stormGateNav = {
 
 export const stormNavigations: NavigationConfig[] = [
 	{
-		title: 'Bench',
-		url: 'https://www.notion.so/stormgate/fe8ab9170c15417fbe25a725c14600e1?v=a5b8bd54102d46a28e82d01d633bd782',
+		title: 'Under Realm',
+		url: 'https://underrealm.stormgate.io',
 	},
 	{
 		title: 'Tokenomic',
