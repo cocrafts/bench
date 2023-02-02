527b1960d0f02b2259cd1ef7630fedb8749ff605
FME849 <1414439@gmail.com>
2023-02-02 18:12:04 +0700

    Add My Profile button to signed in menu

--- src/components/AuthenticationBundle/Signed/Menu.tsx
+++ src/components/AuthenticationBundle/Signed/Menu.tsx
@@ -1,23 +1,45 @@
 import React, { FC } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Hyperlink, modalActions, Text, themeState } from '@metacraft/ui';
+import { Linking, StyleSheet, View } from 'react-native';
+import {
+	Hyperlink,
+	modalActions,
+	ModalConfigs,
+	Text,
+	themeState,
+} from '@metacraft/ui';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { useSnapshot } from 'utils/hook';
 import { signOut } from 'utils/lib';
+import { accountState } from 'utils/state/account';
 
-export const SignedMenu: FC = () => {
+interface Props {
+	config: ModalConfigs;
+}
+
+export const SignedMenu: FC<Props> = ({ config }) => {
 	const { colors } = useSnapshot(themeState);
+	const { profile } = useSnapshot(accountState);
 	const { disconnect } = useWallet();
 
 	const innerSignOut = async () => {
 		await signOut();
 		await disconnect();
-		modalActions.hide('signedOptions');
+		modalActions.hide(config.id as string);
+	};
+
+	const onMyProfilePress = () => {
+		Linking.openURL(`https://stormgate.io/profile/${profile.address}`);
+		modalActions.hide(config.id as string);
 	};
 
 	return (
 		<View style={[styles.container, { backgroundColor: colors.background }]}>
 			<Text style={styles.heading}>Signed Menu</Text>
+			<Hyperlink
+				style={styles.hyperLink}
+				onPress={onMyProfilePress}
+				title="My Profile"
+			/>
 			<Hyperlink
 				style={styles.hyperLink}
 				onPress={innerSignOut}
